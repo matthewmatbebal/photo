b7f4b1f8176fbafb1ab18bbfa27c63c52cff9a4b
matvei.cherntsov <Matvei.cherntsov@gmail.com>
2025-08-25 13:49:27 +0400

    feat: update footer with locale in links

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -3,10 +3,15 @@
 import styles from "./Footer.module.sass";
 import Link from "next/link";
 import { useTranslations } from "@/lib/translations";
+import { buildLocaleHref, getCurrentLocaleFromPath } from "@/utils/localeUtils";
+import { usePathname } from "next/navigation";
+import React from "react";
 
 export default function Footer() {
   const year = new Date().getFullYear();
   const t = useTranslations();
+  const pathname = usePathname() || "/";
+  const currentLocale = getCurrentLocaleFromPath(pathname);
 
   return (
     <footer className={styles.footer}>
@@ -15,8 +20,10 @@ export default function Footer() {
           <div className={styles.description}>{t.footerInfo()}</div>
           <div className={styles.title}>Lyudmila zinchenko</div>
           <div className={styles.navElems}>
-            <Link href="/">{t.gallery()}</Link>
-            <Link href="/about">{t.about()}</Link>
+            <Link href={buildLocaleHref(currentLocale)}>{t.gallery()}</Link>
+            <Link href={buildLocaleHref(currentLocale, "about")}>
+              {t.about()}
+            </Link>
           </div>
         </div>
         <div className={styles.date}>{year}</div>
